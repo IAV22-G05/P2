bd7ccf6abfc1ea99f992058bb1836dec2a08a1ef
Andrés Carnicero <acarni01@ucm.es>
2022-03-04 17:28:48 +0100

    Cambio animación idle-andar

--- Assets/Scripts/MovimientoAutomatico.cs
+++ Assets/Scripts/MovimientoAutomatico.cs
@@ -23,11 +23,12 @@ public class MovimientoAutomatico : MonoBehaviour
 
     //Referencia al Rigidbody
     Rigidbody rb;
-
+    Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -54,6 +55,14 @@ public class MovimientoAutomatico : MonoBehaviour
 
     void Update()
     {
+        //PRUEBAS
+        //Vertex actualVertex = graph.GetNearestVertex(transform.position);
+        //if (actualVertex == vertexObjetive)
+        //{
+        //    idPath++;
+        //    vertexObjetive = path[idPath];
+        //}
+
         //Guardamos pos del vertice
         Vector3 objetivePos = vertexObjetive.gameObject.transform.position;
 
@@ -61,7 +70,7 @@ public class MovimientoAutomatico : MonoBehaviour
         Vector3 direccion;
         direccion = objetivePos - transform.position;
         Debug.Log(objetivePos.ToString());
-        rb.velocity = direccion*10;
+        rb.velocity = direccion * 10;
 
         //Si llegamos al nodo objetivo, pasamos al siguiente
         //Hay que cambiar el margen
@@ -70,14 +79,20 @@ public class MovimientoAutomatico : MonoBehaviour
             && transform.position.z <= objetivePos.z + 0.1
             && transform.position.z >= objetivePos.z - 0.1)
         {
+
             idPath++;
             vertexObjetive = path[idPath];
-
         }
 
         //Orientacion
         direccion.y = 0;
         transform.rotation = Quaternion.LookRotation(direccion, Vector3.up);
+
+        //Animacion
+        if (direccion.magnitude > 0)
+            anim.SetBool("walk", true);
+        else
+            anim.SetBool("walk", false);
 
     }
 
